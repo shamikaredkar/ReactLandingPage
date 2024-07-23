c65b27da3956f61362e87a08177b4450ec3ef202
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-23 15:03:33 -0400

    Revised padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import CoreConcepts from "./components/Props";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
@@ -31,6 +32,15 @@ function App() {
               description={CORE_CONCEPTS[3].description}
             />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples: </h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
